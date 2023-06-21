e35162bdbcc5cf7f9479a3197ab91c86f27bbd5a
AlfitoDwiPutra <alfitoputra09@gmail.com>
2023-06-21 09:50:24 +0700

    Perubahan pada BilanganActivity.java dan activity_bilangan.xml

--- app/src/main/java/com/sugiartha/juniorandroid/BilanganActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/BilanganActivity.java
@@ -1,36 +1,56 @@
 package com.sugiartha.juniorandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BilanganActivity extends AppCompatActivity {
 
-    Button proses;
-    EditText angka;
-    TextView bilangan;
+    private EditText angka;
+    private TextView bilangan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bilangan);
 
-        proses = (Button) findViewById(R.id.proses);
-        angka = (EditText) findViewById(R.id.angka);
-        bilangan = (TextView) findViewById(R.id.bilangan);
+        angka = findViewById(R.id.angka);
+        bilangan = findViewById(R.id.bilangan);
 
-        proses.setOnClickListener(new Button.OnClickListener() {
-            @Override public void onClick(View v) {
-                int nilai = Integer.parseInt(angka.getText().toString());
-                if ((nilai%2) == 0){
-                    bilangan.setText("Bilangan Genap");
-                }else{
-                    bilangan.setText("Bilangan Ganjil");
-                }
-            }});
+        Button prosesButton = findViewById(R.id.proses);
+        prosesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prosesAngka();
+            }
+        });
+    }
+
+    private void prosesAngka() {
+        String input = angka.getText().toString();
+        if (input.isEmpty()) {
+            Toast.makeText(this, "Masukkan angka terlebih dahulu", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            int nilai = Integer.parseInt(input);
+            String hasil;
+            if (nilai % 2 == 0) {
+                hasil = "Bilangan Genap";
+            } else {
+                hasil = "Bilangan Ganjil";
+            }
+            String output = input + " Adalah " + hasil;
+            bilangan.setText(output);
+            bilangan.setVisibility(View.VISIBLE);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Masukkan angka yang valid", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
