b83b999d0adf9c6581c1b2c8667b8f73036c98cf
AlfitoDwiPutra <alfitoputra09@gmail.com>
2023-06-23 19:14:02 +0700

    perubahan listview, sqlite, splashscreen

--- app/src/main/java/com/sugiartha/juniorandroid/ListViewActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/ListViewActivity.java
@@ -2,8 +2,12 @@ package com.sugiartha.juniorandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListViewActivity extends AppCompatActivity {
 
@@ -14,7 +18,7 @@ public class ListViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
 
-        lvResult = (ListView) findViewById(R.id.lvResult);
+        lvResult = findViewById(R.id.lvResult);
 
         String[][] dataNegara = new String[][]{{"Indonesia", "ASIA", "indonesia"},
                 {"Malaysia", "ASIA", "malaysia"}, {"Singapore", "ASIA", "singapore"},
