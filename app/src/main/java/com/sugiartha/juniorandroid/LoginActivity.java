e8cdaee91056bcdb6a9f7d584b71c5f1932199a0
dikafdp <ikawidyaningsih5@gmail.com>
2023-06-26 23:01:43 +0700

    ubah tampilan main activity dan catatan

--- app/src/main/java/com/sugiartha/juniorandroid/LoginActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/LoginActivity.java
@@ -2,13 +2,27 @@ package com.sugiartha.juniorandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnlogin = (Button) findViewById(R.id.btn_login);
+
+        btnlogin.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
